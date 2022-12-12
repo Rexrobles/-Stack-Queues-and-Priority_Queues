567655a759b43b8ec928d1a98295ddef7d0bbc30
Rex-Imman Robles <reximman03@gmail.com>
2022-12-13 00:14:53 +0800

    adding enqueue as append and dequeue as popleft

--- queues.py
+++ queues.py
@@ -7,4 +7,10 @@ from collections import deque
 # Class variables for the implementation of enqueue and dequeue.
 class Queue:
     def __init__(self):
-        self._elements = deque() 
+        self._elements = deque() # The leading underscore in the attribute’s name indicates an internal bit of implementation, which only the class should access and modify.
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
