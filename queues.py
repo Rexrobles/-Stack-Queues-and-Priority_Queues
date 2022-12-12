c864c5c0b51d9243014779587e175f667f5830dc
Rex-Imman Robles <reximman03@gmail.com>
2022-12-13 00:12:14 +0800

    adding a class on the elements for enqueue and dequeue function.

--- queues.py
+++ queues.py
@@ -4,3 +4,7 @@
 # Importing the Queues module 
 from collections import deque
 
+# Class variables for the implementation of enqueue and dequeue.
+class Queue:
+    def __init__(self):
+        self._elements = deque() 
