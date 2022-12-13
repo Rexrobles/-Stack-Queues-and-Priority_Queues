5d31e43ef8c651fede229d9ba5cba940cbbfc25e
Rexrobles <reximma03@gmail.com>
2022-12-14 01:39:48 +0800

    adding variables

--- queuesPRIO.py
+++ queuesPRIO.py
@@ -3,12 +3,11 @@
 # Importing the required Python module for the priority queues
 from collections import deque 
 from heapq import heappop, heappush
-from queues import PriorityQueue
 
 class PriorityQueue:
     # Init function for the Priority Queues 
     def __init__(self):
-        self._elements = []
+        self.elements = []
         
     # adding an element into the Priority Queue.
     def enqueue_with_priority(self, priority, value):
@@ -17,3 +16,15 @@ class PriorityQueue:
     # Heappop for dequeueing elements from the Priority Queue.
     def dequeue(self):
         return heappop(self.elements)
+    
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+messages.dequeue()
