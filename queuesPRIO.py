f36e49a68656fa272d03b24f83c8b993ae4eb7aa
Rexrobles <reximma03@gmail.com>
2022-12-13 21:33:22 +0800

    creating a file for priority queues

---
+++ queuesPRIO.py
@@ -0,0 +1,6 @@
+# Priority Queues Python File
+
+# The purpose of a heap is not so much to sort elements as it is to store 
+# them in a certain relationship to allow for fast lookup.
+# importing heappush
+from heapq import heappush
