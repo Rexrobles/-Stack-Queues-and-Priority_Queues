8e7cf1593051876724b5d7961db1c64355c95a38
Rexrobles <reximma03@gmail.com>
2022-12-14 00:58:53 +0800

    adding a class variable

--- queuesPRIO.py
+++ queuesPRIO.py
@@ -3,3 +3,8 @@
 # Importing the required Python module for the priority queues
 from collections import deque 
 from heapq import heappop, heappush
+
+class PriorityQueue:
+    # Init function for the Priority Queues 
+    def __init__(self):
+        self._elements = []
