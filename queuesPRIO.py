5d91a42e1d25d478a3c6ba152a3957fbbfae1264
Rexrobles <reximma03@gmail.com>
2022-12-14 01:03:27 +0800

    importing PriorityQueue and adding some comment

--- queuesPRIO.py
+++ queuesPRIO.py
@@ -3,8 +3,17 @@
 # Importing the required Python module for the priority queues
 from collections import deque 
 from heapq import heappop, heappush
+from queues import PriorityQueue
 
 class PriorityQueue:
     # Init function for the Priority Queues 
     def __init__(self):
         self._elements = []
+        
+    # adding an element into the Priority Queue.
+    def enqueue_with_priority(self, priority, value):
+        heappush(self.elements, (priority, value))
+
+    # Heappop for dequeueing elements from the Priority Queue.
+    def dequeue(self):
+        return heappop(self.elements)
