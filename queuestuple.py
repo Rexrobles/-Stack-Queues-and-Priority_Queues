7aa9193fda04d4ff8d21819d930222361c93c1ec
Rexrobles <reximma03@gmail.com>
2022-12-14 00:11:03 +0800

    adding elif

--- queuestuple.py
+++ queuestuple.py
@@ -3,3 +3,8 @@
 person1 =  ("Rex", "Brown", 23)
 person2 =  ("Rex", "Brown", 24)
 person3 =  ("Rex", "Brown", 23)
+
+if person1 < person2:
+    print("True")
+elif person2 < person3:
+    print("False")
