c51deb2fcfbe10325c1c113df03aaf895f965896
Rexrobles <reximma03@gmail.com>
2022-12-14 00:13:48 +0800

    adding a comment

--- queuestuple.py
+++ queuestuple.py
@@ -4,6 +4,7 @@ person1 =  ("Rex", "Brown", 23)
 person2 =  ("Rex", "Brown", 24)
 person3 =  ("Rex", "Brown", 23)
 
+# adding elif function to see the comparison
 if person1 < person2:
     print("True")
 elif person2 < person3:
