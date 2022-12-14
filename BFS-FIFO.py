80291a1762965b6ea178967cbb3b77049e24ca71
Rexrobles <reximma03@gmail.com>
2022-12-15 01:45:55 +0800

    For loop for nx.bfs_tree()

--- BFS-FIFO.py
+++ BFS-FIFO.py
@@ -8,3 +8,12 @@ from graph import City, load_graph
 # This def function argument will returning a value that are being considered between the 20th century.
 def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+for node in nx.bfs_tree(graph, nodes["chester"]):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
