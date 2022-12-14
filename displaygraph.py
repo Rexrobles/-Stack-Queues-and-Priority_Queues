80d108302e389e76cbc4f49abf53a7433622f656
Rexrobles <reximma03@gmail.com>
2022-12-15 00:21:57 +0800

    file inheritance from graph.py

--- displaygraph.py
+++ displaygraph.py
@@ -1,1 +1,6 @@
 # This program will load the graph from a sample DOT file and store the result in two variables.
+from graph import City, load_graph
+
+# The load_graph inherits parameters from the graph.py file.
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+print(f"\n{nodes['london']}\n")
