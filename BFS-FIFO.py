708664f65d4a888428499d209eaba4fc9ddab4f7
Rexrobles <reximma03@gmail.com>
2022-12-15 01:18:52 +0800

    Creating Breadth-First Search Using a FIFO Queue files

---
+++ BFS-FIFO.py
@@ -0,0 +1,3 @@
+# Breadth-First Search Using a FIFO Queue
+# it look for a node that satisfies a particular condition by exploring the graph in concentric layers or levels.
+# Using chosen source node unless the graph is a tree data structure, in which case you typically start at the root node of that tree. 
