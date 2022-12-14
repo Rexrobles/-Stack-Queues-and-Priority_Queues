5bd89eec16e175cceb2b5293c29140ea2df419c9
Rexrobles <reximma03@gmail.com>
2022-12-15 01:07:06 +0800

    remove another code for redundantcy and add code that will sort the the distance in ascending order

--- displaygraph.py
+++ displaygraph.py
@@ -6,5 +6,11 @@ nodes, graph = load_graph("roadmap.dot", City.from_dict)
 print(f"\n{nodes['chester']}")
 print(f"\n{graph}\n")
     
-for neighbor, weights in graph[nodes["chester"]].items():
-    print(weights["distance"], neighbor.name)
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+def by_distance(weights):
+    return float(weights["distance"])
+
+for neighbor, weights in sort_by(graph[nodes["chester"]], by_distance):
+    print(f"{weights['distance']:>3} miles, {neighbor.name}")
