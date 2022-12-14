78588b640777e679b62c465596a99f8b9f3f5350
Rexrobles <reximma03@gmail.com>
2022-12-14 12:16:27 +0800

    adding class variables

--- queuesHCC.py
+++ queuesHCC.py
@@ -7,3 +7,9 @@ from dataclasses import dataclass
 @dataclass
 class message:
     event: str
+    
+# class variable for implementation that have same a same priority
+wipers = message("Windshield wipers turned on")
+hazard_lights = message("Hazard lights turned on")
+
+wipers < hazard_lights
