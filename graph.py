a982c09d017b047dc8efc3cc0cf72a0ca1f0436f
Rexrobles <reximma03@gmail.com>
2022-12-14 22:42:34 +0800

    adding data class

--- graph.py
+++ graph.py
@@ -1,3 +1,10 @@
 # Creating a custom data type for conveniently viewing graphs in cities and road maps.
 
 from typing import NamedTuple
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
