10b1cce279c48d43099cd326f159ffbcbfcc779c
Rexrobles <reximma03@gmail.com>
2022-12-14 22:47:22 +0800

    adding @classmethod

--- graph.py
+++ graph.py
@@ -2,9 +2,21 @@
 
 from typing import NamedTuple
 
+# Adding data class for future use, such as the networkx need.
 class City(NamedTuple):
     name: str
     country: str
     year: int | None
     latitude: float
     longitude: float
+    
+    # extending a named tuple to ensure that your node objects are hashable, which is required by networkx.
+    @classmethod
+    def from_dict(cls, attributes):
+        return cls(
+            name = attributes["xlabel"],
+            country = attributes["country"],
+            year = int(attributes["year"]) or None,
+            latitude = float(attributes["latitude"]),
+            longitude = float(attributes["longitude"]),
+        )
