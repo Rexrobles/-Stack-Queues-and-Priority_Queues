675d3682c5d0f85a342b1061ce97d26849d5048b
Rexrobles <reximma03@gmail.com>
2022-12-15 01:28:26 +0800

    Importing module networkx will help the program on the specified algorithms

--- BFS-FIFO.py
+++ BFS-FIFO.py
@@ -1,3 +1,5 @@
 # Breadth-First Search Using a FIFO Queue
 # it look for a node that satisfies a particular condition by exploring the graph in concentric layers or levels.
 # Using chosen source node unless the graph is a tree data structure, in which case you typically start at the root node of that tree. 
+
+import networkx as nx
