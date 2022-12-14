cfc69021c2d5f94a2cb2da465480225f3394eb34
Rexrobles <reximma03@gmail.com>
2022-12-15 01:33:36 +0800

    added def function argument

--- BFS-FIFO.py
+++ BFS-FIFO.py
@@ -3,3 +3,7 @@
 # Using chosen source node unless the graph is a tree data structure, in which case you typically start at the root node of that tree. 
 
 import networkx as nx
+from graph import City, load_graph
+
+def is_twentieth_century(year):
+    return year and 1901 <= year <= 2000
