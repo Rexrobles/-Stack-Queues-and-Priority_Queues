f60fb70778ec36a8481b1a250dbe0c956af2a6bb
Rexrobles <reximma03@gmail.com>
2022-12-14 17:43:36 +0800

    testing one map string

--- roadmap.py
+++ roadmap.py
@@ -3,4 +3,7 @@
 # importing networkx
 import networkx as nx 
 
-print(nx.nx_agraph.read_dot("roadmap.dot"))
+# try print one map string from roadmap.dot if it will correspond to the dictionary keyvalue
+graph = nx.nx_agraph.read_dot("roadmap.dot")
+print(f"\n{graph}")
+print(f"{graph.nodes['chester']}\n")
