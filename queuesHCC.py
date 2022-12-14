0cbbc247cc33bbc8b91a39e3eb04fb126e2aa412
Rexrobles <reximma03@gmail.com>
2022-12-14 12:12:10 +0800

    importing data class module and class to represent the message

--- queuesHCC.py
+++ queuesHCC.py
@@ -1,1 +1,9 @@
 # Handling Corner Cases (HCC) Priority Queues Python File
+
+# Imported data class module 
+from dataclasses import dataclass
+
+# class to represent messages in the queue.
+@dataclass
+class message:
+    event: str
