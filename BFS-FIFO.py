038da4eef7a280c7edfefaa335c2e75c7faa5b14
Rexrobles <reximma03@gmail.com>
2022-12-15 01:58:30 +0800

    changing node[chester] to node[edinburgh]

--- BFS-FIFO.py
+++ BFS-FIFO.py
@@ -10,7 +10,7 @@ def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
-for node in nx.bfs_tree(graph, nodes["chester"]):
+for node in nx.bfs_tree(graph, nodes["edinburgh"]):
     print("📌", node.name)
     if is_twentieth_century(node.year):
         print("\n✔️  Found:", node.name, node.year,"\n")
