7fa93f0e719370611aad267341a518baae8ef057
Rexrobles <reximma03@gmail.com>
2022-12-17 05:04:14 +0800

    add QUEUE_TYPES dictionary

--- Thread-Safe.py
+++ Thread-Safe.py
@@ -3,3 +3,9 @@
 # Importing the necessary module and queue classes into the global namespace.
 import argparse
 from queue import LifoQueue, PriorityQueue, Queue
+
+QUEUE_TYPES = {
+    "fifo": Queue,
+    "lifo": LifoQueue,
+    "heap": PriorityQueue
+}
