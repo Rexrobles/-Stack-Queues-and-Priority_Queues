9988e2a0fffe9979f3fba19bbd2890e99302a9a1
Rexrobles <reximma03@gmail.com>
2022-12-16 20:15:54 +0800

    add some print func() for better output

--- travers.py
+++ travers.py
@@ -10,7 +10,8 @@ def is_twentieth_century(city):
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 city = bfs(graph, nodes["edinburgh"], is_twentieth_century)
-print(city.name)
+print(f"City in the 20th century is", city.name)
 
+print("Neighboring cities are:")
 for city in breadth_first_traverse(graph, nodes["edinburgh"]):
     print(city.name)
