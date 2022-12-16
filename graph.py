3cc52bf93f8cd60227af7584188ca6a6e6125246
Rexrobles <reximma03@gmail.com>
2022-12-17 00:41:54 +0800

    remove the modified code so that it will not cause some error to travers.py

--- graph.py
+++ graph.py
@@ -35,21 +35,18 @@ def load_graph(filename, node_factory):
         for name1, name2, weights in graph.edges(data=True)
     )
     
-def breadth_first_traverse(graph, source, order_by=None):
+def breadth_first_traverse(graph, source):
     queue = Queue(source)
     visited = {source}
     while queue:
         yield (node := queue.dequeue())
-        neighbors = list(graph.neighbors(node))
-        if order_by:
-            neighbors.sort(key=order_by)
-        for neighbor in neighbors:
+        for neighbor in graph.neighbors(node):
             if neighbor not in visited:
                 visited.add(neighbor)
                 queue.enqueue(neighbor)
 
-def breadth_first_search(graph, source, predicate, order_by=None):
-    for node in breadth_first_traverse(graph, source, order_by):
+def breadth_first_search(graph, source, predicate):
+    for node in breadth_first_traverse(graph, source):
         if predicate(node):
             return node
         
