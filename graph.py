5561d78bdfdab5f8c324121882ff89ef026c4694
Rexrobles <reximma03@gmail.com>
2022-12-17 01:31:32 +0800

    add def function that can tell you whether two nodes remain connected or not.

--- graph.py
+++ graph.py
@@ -94,3 +94,6 @@ def retrace(previous, source, destination):
 
     path.appendleft(source)
     return list(path)
+
+def connected(graph, source, destination):
+    return shortest_path(graph, source, destination) is not None
