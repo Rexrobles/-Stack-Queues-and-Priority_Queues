e2e9f3c943ae2885f6692aacba33b3996da5b2d5
Rexrobles <reximma03@gmail.com>
2022-12-17 00:50:53 +0800

    creating a code that will display the shortest path

---
+++ shortest-BFT.py
@@ -0,0 +1,11 @@
+# importing networkx to reveal all the shortest paths between two cities, which will have the same minimal length
+import networkx as nx
+from graph import City, load_graph
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+city1 = nodes["aberdeen"]
+city2 = nodes["perth"]
+
+for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+    print(f"{i}.", " → ".join(city.name for city in path))
